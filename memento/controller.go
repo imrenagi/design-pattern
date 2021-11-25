512e44cb43a4f48b3a82c1d8392c3c6abfc33392
Imre Nagi <imre.nagi@go-jek.com>
2021-11-25 22:19:26 +0700

    Add cmd shift z (redo) operation with memento
    Signed-off-by: Imre Nagi <imre.nagi@go-jek.com>

--- memento/controller.go
+++ memento/controller.go
@@ -1,24 +1,40 @@
 package memento
 
+import "fmt"
+
 type Controller struct {
   Editor *Editor
   Histories []Memento
+
+  // ptr is pointer to the current snapshot being used
+  ptr int
 }
 
 func (c *Controller) CmdSave() {
   m := c.Editor.Save()
   c.Histories = append(c.Histories, m)
+
+  l := len(c.Histories)
+  c.ptr = l-1
+
+  fmt.Println("current pointer ", c.ptr)
 }
 
 func (c *Controller) CmdUndo() {
-  l := len(c.Histories)
-  if l > 0 {
-    m := c.Histories[l-1]
-    c.Histories = c.Histories[:l-1]
+    c.ptr -= 1
+    m := c.Histories[c.ptr]
     c.Editor.Restore(m)
-  }
+    fmt.Println("undo current pointer ", c.ptr)
 }
-//
-// func (c *Controller) CmdRedo() {
-//
-// }
+
+func (c *Controller) CmdRedo() {
+  l := len(c.Histories)
+  if (c.ptr + 1) >= l {
+    return
+  }
+  c.ptr++
+  m := c.Histories[c.ptr]
+  c.Editor.Restore(m)
+
+  fmt.Println("redo current pointer ", c.ptr)
+}
--- memento/executor.go
+++ memento/executor.go
@@ -25,9 +25,27 @@ func Execute() {
 
   fmt.Println(editor.Text)
 
-  controller.CmdUndo()
+  controller.CmdRedo()
 
   fmt.Println(editor.Text)
+
+  controller.CmdRedo()
+  controller.CmdRedo()
+
+  fmt.Println(editor.Text)
+
+  //
+  // controller.CmdSave()
+  //
+  // fmt.Println(editor.Text)
+  //
+  // controller.CmdUndo()
+  //
+  // fmt.Println(editor.Text)
+  //
+  // controller.CmdUndo()
+  //
+  // fmt.Println(editor.Text)
 
 
 
